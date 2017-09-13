0bad73f688efdad203edc5b91687212745c6e118
Andrew Peebles <aqpeeb@gmail.com>
2017-09-12 22:43:44 -0700

    switch to using axios

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,51 +1,31 @@
 // Utilities for calling backend apis
+import axios from 'axios';
 
 export function postJSON(url, data) {
   return new Promise((resolve, reject) => {
-    let opts = {
-      method: 'post',
-      headers: {
-	'Accept': 'application/json, text/plain, */*',
-	'Content-Type': 'application/json'
-      },
-      body: JSON.stringify( data )
-    };
-    window.fetch(url, opts)
-          .then(res => {
-            if (res.ok) {
-              res.json().then(resolve).catch(resolve)
-            } else {
-              res.text().then((text) => reject({status: res.status, message: text || res.statusText, statusText: res.statusText, responseText: text})).catch( reject );
-            }
-          })
-          .catch(reject)
+    axios.post( url, data ).then( (response) => {
+      resolve( response.data );
+    }).catch( (err) => {
+      if ( err.response )
+	reject({ status: err.response.status, message: err.response.data || err.response.statusText });
+      else
+	reject({ status: 500, message: err.message });
+    });
   });
 }
 
-const queryParams = (params) => {
-  return Object.keys(params)
-               .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-               .join('&');
-}
-
-export function getJSON(url, params) {
+export function getJSON(url, data) {
   return new Promise((resolve, reject) => {
-    let opts = {
-      method: 'get',
-      headers: {
-	'Accept': 'application/json, text/plain, */*',
-      }
-    };
-    url += (url.indexOf('?') === -1 ? '?' : '&') + queryParams( params );
-    window.fetch(url, opts)
-          .then(res => {
-            if (res.ok) {
-              res.json().then(resolve).catch(resolve)
-            } else {
-              res.text().then((text) => reject({status: res.status, message: text || res.statusText, statusText: res.statusText, responseText: text})).catch( reject );
-            }
-          })
-          .catch(reject)
+    let qs;
+    if ( data ) qs = { params: data };
+    axios.get( url, qs ).then( (response) => {
+      resolve( response.data );
+    }).catch( (err) => {
+      if ( err.response )
+	reject({ status: err.response.status, message: err.response.data || err.response.statusText });
+      else
+	reject({ status: 500, message: err.message });
+    });    
   });
 }
 
